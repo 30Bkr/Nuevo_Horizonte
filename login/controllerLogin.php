cbb2702683d2fdc94073f529e137c4552c34e4f9
Sylnex <neudelys03@gmail.com>
2025-11-02 16:47:01 -0400

    cambios el controller

--- login/controllerLogin.php
+++ login/controllerLogin.php
@@ -9,10 +9,12 @@ $contrasena = password_hash($_POST['password'], PASSWORD_BCRYPT, ['cost' => 12])
 $email = $_POST["email"];
 $user = new Usuarios;
 $listaPersona = $user->consultar($email);
-// $info = $user->info($listaPersona[0]->nombre_usuario);
+$info = $user->info($listaPersona[0]->nombre_usuario);
 
 // echo "<pre>";
 // var_dump($user->consultar($email));
+// var_dump($listaPersona[0]->nombre_usuario);
+
 // var_dump($listaPersona[0]->contraseña);
 // var_dump($listaPersona[0]->id_persona);
 // var_dump($info);
@@ -23,7 +25,7 @@ $listaPersona = $user->consultar($email);
 // echo "<pre>";
 // var_dump($listaPersona);
 // echo "</pre>";
-if (password_verify($listaPersona[0]->contraseña, $contrasena) && ($listaPersona[0]->estado_cuenta === 0)) {
+if (password_verify($listaPersona[0]->contraseña, $contrasena) && ($listaPersona[0]->estatus === 1)) {
   echo "aca2 si es la misma";
   session_start();
   header('Location:' . URL . '/admin/index.php');
