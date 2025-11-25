0a630f56cc17c771945e9529d05c0cf638b64990
The Briant Carrillo <brianttd24.7@gmail.com>
2025-11-24 22:16:56 -0400

    elminamos bases de datos que no usamos

--- layout/layaout1.php
+++ layout/layaout1.php
@@ -3,7 +3,7 @@ include_once('/xampp/htdocs/final/global/utils.php');
 include_once('/xampp/htdocs/final/app/users.php');
 
 if (session_status() === PHP_SESSION_NONE) {
-    session_start();
+  session_start();
 }
 $esto = $_SESSION['sesion_email'];
 $user = new Usuarios;
@@ -159,7 +159,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
                   </a>
                 </li>
                 <li class="nav-item">
-                  <a href="<?= URL; ?>/admin/inscripciones/plop.php" class="nav-link">
+                  <a href="<?= URL; ?>/admin/reinscripciones/reinscripcion.php" class="nav-link">
                     <i class="far fa-circle nav-icon"></i>
                     <p>Re-inscripcion</p>
                   </a>
