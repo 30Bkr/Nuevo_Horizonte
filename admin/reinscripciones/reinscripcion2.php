9c7e68b8a0538ad8224fb1340eaa5712fbc4218d
The Briant Carrillo <brianttd24.7@gmail.com>
2025-12-07 17:18:46 -0400

    modificamos manejos de periodos

--- admin/reinscripciones/reinscripcion2.php
+++ admin/reinscripciones/reinscripcion2.php
@@ -736,6 +736,8 @@ try {
                             {$periodo['descripcion_periodo']} ({$fecha_ini} al {$fecha_fin}) {$periodo['estatus']}
                           </option>";
                                 }
+                              } else if ($periodos == []) {
+                                echo "<option value=''>El periodo academico activo ha finalizado</option>";
                               } else {
                                 echo "<option value=''>No hay períodos vigentes</option>";
                               }
@@ -745,7 +747,9 @@ try {
                               <?php
                               $fecha_hoy = date('d/m/Y');
                               if (!empty($periodos)) {
-                                echo "Fecha actual: {$fecha_hoy} - Períodos dentro del rango de fechas";
+                                echo "Fecha actual: {$fecha_hoy} - Períodos dentro del rango de fechas.";
+                              } else  if ($periodos == []) {
+                                echo '⚠️ No es posible reinscribir. El periodo academico activo ha finalizado.';
                               } else {
                                 echo "⚠️ No hay períodos académicos vigentes. Fecha actual: {$fecha_hoy}";
                               }
